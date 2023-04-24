856dcd057f27d967c4f0e5a083eb819b7859899e
Katoo_Roofthooft <r0929442@student.thomasmore.be>
2023-04-24 15:22:54 +0200

    added info to index page

--- website/script.js
+++ website/script.js
@@ -12,23 +12,45 @@ fetch("api_books.json")
             cover.alt = book.title;
             bookDiv.appendChild(cover);
 
-            // const infoDiv = document.createElement("div");
-            // infoDiv.classList.add("book-info");
+            const infoDiv = document.createElement("div");
+            infoDiv.classList.add("book-info");
 
             const title = document.createElement("h3");
             bookDiv.classList.add("book-div");
             title.classList.add("book-title");
 
-            title.textContent = book.title;
-            // infoDiv.appendChild(title);
+            title.textContent = book.title + " (" + book.publication_date + ")";
+            infoDiv.appendChild(title);
 
             const author = document.createElement("p");
             author.classList.add("book-author");
 
             author.textContent = book.author;
-            // infoDiv.appendChild(author);
+            infoDiv.appendChild(author);
 
-            // bookDiv.appendChild(infoDiv);
+            const genre = document.createElement("h3");
+            bookDiv.classList.add("book-div");
+            genre.classList.add("book-genre");
+
+            genre.textContent = book.genre;
+            infoDiv.appendChild(genre);
+
+            const language = document.createElement("h3");
+            bookDiv.classList.add("book-div");
+            language.classList.add("book-language");
+
+            language.textContent = book.language;
+            infoDiv.appendChild(language);
+
+            const summary = document.createElement("h3");
+            bookDiv.classList.add("book-div");
+            summary.classList.add("book-summary");
+
+            summary.textContent = book.summary;
+            infoDiv.appendChild(summary);
+
+            bookDiv.appendChild(infoDiv);
             booksContainer.appendChild(bookDiv);
+
         }
     });
